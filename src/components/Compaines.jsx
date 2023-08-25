070f5def5cab9c6e8647b27ccbe72355ddc9c798
AsadBulediReal <bulediasadjamil@gmail.com>
2023-08-25 14:42:56 +0500

    added companies section

---
+++ src/components/Compaines.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../assets/styles/Compaines.css";
+
+const Compaines = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="flexCenter paddings innerWidth c-container">
+        <img src="./prologis.png" alt="prologis" />
+        <img src="./tower.png" alt="tower" />
+        <img src="./equinix.png" alt="equinix" />
+        <img src="./realty.png" alt="realty" />
+      </div>
+    </section>
+  );
+};
+
+export default Compaines;

fdd03cee469607b84e00e3e8eba76f9b0b35af49
AsadBulediReal <bulediasadjamil@gmail.com>
2023-08-25 06:04:22 +0500

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
-
+import Hero from "./components/Hero";
+import "./assets/styles/App.css";
 function App() {
   return (
     <div className="app">
-      <Header />
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,60 @@
 import React from "react";
-
+import "../assets/styles/Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 const Hero = () => {
-  return <div>Hero</div>;
-};
+  return (
+    <section className="hero-wrapper">
+      <div className="flexCenter paddings innerWidth hero-container">
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br /> Most Suitable
+              <br /> Property
+            </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <p className="secondaryText">Find a variety of properties that suit you very easilty</p>
+            <p className="secondaryText">Forget all difficulties in finding a residence for you</p>
+          </div>
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" placeholder="Search by title/city/country" />
+            <button className="btn">Search</button>
+          </div>
 
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={0} end={9000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>{" "}
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={0} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>{" "}
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={0} end={28} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winning</span>
+            </div>
+          </div>
+        </div>
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="Hero Image" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Hero;
